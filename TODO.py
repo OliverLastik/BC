d0ad465a6dfd1862848a8d8465a81b08750a5156
OliverL <119365594+OliverLastik@users.noreply.github.com>
2024-06-06 21:40:12 +0200

    Delete TODO.py

--- TODO.py
+++
@@ -1,5 +0,0 @@
-##Umele zobratie datasetu z origo imagu a rescale na 299x299 TICK
-# Implementing Cost-sensitive Learning
-# Radial Histogram
-# Diff Backbone TICK
-# Test adjusting the InceptionV3 model/VGG16 model for at least +-90%
